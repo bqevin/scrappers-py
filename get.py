376cd382bb743ecea7b2138f09d701fdd555564b
Kevin Barasa <kevin.barasa001@gmail.com>
2018-03-28 18:10:46 +0300

    set encoding type

--- get.py
+++ get.py
@@ -1,10 +1,19 @@
-from bs4 import BeautifulSoup
+# -*- coding: utf-8 -*-
 import urllib2
+from bs4 import BeautifulSoup
 
 mailmix_str = "http://webshop.mailmix.nl/catalog/category/"
-
 content = urllib2.urlopen(mailmix_str).read()
+soup = BeautifulSoup(content, 'html.parser')
 
-soup = BeautifulSoup(content)
+data = []
+my_categories = soup.find_all("li", {"class": "level0"})
 
-print soup.prettify()
+# data.append((things))
+# with open(‘index.csv’, ‘a’) as csv_file:
+#     writer = csv.writer(csv_file)
+# writer.writerow([name, price, datetime.now()])
+
+for category in my_categories:
+    print(category.find('a').text.strip())
+
