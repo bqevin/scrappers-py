cb9e30bcee55742843efbd67dc102c57d6b70da3
Kevin Barasa <kevin.barasa001@gmail.com>
2018-04-03 11:17:42 +0300

    complete the mailmix fetch

--- get_mailmix.py
+++ get_mailmix.py
@@ -14,12 +14,7 @@ def url_parser(url):
     return soup
 
 
-html_content = url_parser(mail_mix_str)
-
-my_categories = html_content.find_all("li", {"class": "level0"})
-
-
-# Get products in any category provided a URL
+# Get products in provided a URL
 def products_loop(url):
     my_products = url_parser(url)
     for item in my_products.find_all('div', {'class': 'item-container'}):
@@ -36,16 +31,17 @@ def products_loop(url):
         fabric_code = ''
         if check_fabric_code is not None:
             fabric_code = check_fabric_code.text
-        # check_article_number = single_item.find('table', {'class': 'data-table'}).find('td', {'class': 'data last'})
-        # article_number = ''
-        # if check_article_number is not None:
-        #     article_number = check_article_number.text
-        print(item_title)
+        check_article_number = single_item.find('table', {'class': 'data-table'})
+        article_number = ''
+        if check_article_number is not None:
+            article_number = check_article_number.text.strip()
+            article_number = article_number.split('\n')[1]
+        print(brand_name)
         data.append([
             product_desc,
             image_src,
             fabric_code,
-            # article_number,
+            article_number,
             brand_name,
             cat_title,
             sub_title,
@@ -55,45 +51,44 @@ def products_loop(url):
 
 def get_products(url):
     this_page = url_parser(url)
-    #initial first page
+    #first page crawling
     products_loop(url)
     if this_page.find('div', {'class': 'pages'}) is not None:
         pages = this_page.find('div', {'class': 'pages'}).find_all('a')
         stage = 0
         for page in pages:
             stage = stage + 1
-            # skip using href in first loop
+            # skip crawling 1st page URL again
             if stage >= 2:
                 current_page_url = page['href']
                 products_loop(current_page_url)
 
 
+#main homepage
+html_content = url_parser(mail_mix_str)
+my_categories = html_content.find_all("li", {"class": "level0"})
+
 for category in my_categories:
     cat_title = category.find('a').text.strip()
     sub_cat = category.find_all('li', {'class': 'level1'})
-    print(cat_title)
-
     # Loop through the subcategories
     for sub in sub_cat:
         sub_href = sub.find('a')['href']
         sub_title = sub.find('a').text.strip()
         under_title = ''
-        print(sub_title)
-
+        print(cat_title + ' -> ' + sub_title)
         # find products -> single product
         get_products(sub_href)
-
         # if there is under cat - go step deeper
         if sub.find('ul', {'class': 'level1'}):
             under_sub = sub.find_all('li', {'class': 'level2'})
             for _under in under_sub:
                 under_href = _under.find('a')['href']
                 under_title = _under.find('a').text.strip()
-                print(under_title)
-
+                print(cat_title + ' -> ' + under_title)
                 # find products -> single product
                 get_products(under_href)
-    print('----------------')
+    print(cat_title + '-- DONE --')
 
 with open('mailmix.csv', 'wb') as csv_file:
     writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
